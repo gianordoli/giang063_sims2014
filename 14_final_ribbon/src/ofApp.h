13ad50ae988d8e3331a873c3d8abe434b695cad0
gianordoli <gianordoligabriel@yahoo.com.br>
2014-12-07 22:52:59 -0500

    Revert 8d8a43a..e231a10
    This rolls back to commit 8d8a43a17ab1f2f0cc06bafd3246865434b4e610.

--- 14_final_ribbon/src/ofApp.h
+++ 14_final_ribbon/src/ofApp.h
@@ -33,6 +33,14 @@ public:
     bool isErasing;
     void eraseShapes();
     
+    /*-------------------- GUI --------------------*/
+    ofxUISuperCanvas *gui;
+    void guiEvent(ofxUIEventArgs &e);
+    void setGUI();
+    vector<string> modes;
+    string selectedMode;
+    
+    
     /*----------------- PLAYBACK -------------------*/
     int totalVertices;
     float playbackSlider;
@@ -67,19 +75,6 @@ public:
     float amplitude;
     int frequencyInSeconds;
     int nModifier;
-    bool isOscillating;
-    
-    /*-------------------- GUI --------------------*/
-    ofxUISuperCanvas * gui;
-//    ofxUISuperCanvas * gui2;    
-    
-    void guiEvent(ofxUIEventArgs &e);
-    void setGUI();
-    vector<string> modes;
-    string selectedMode;
-    
-    void setGUI2();
-    //    ofxUISuperCanvas *guiOscillation;
-    void setGUISOscillation();
+    bool isOscillating;    
 
 };
